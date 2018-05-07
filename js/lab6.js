c6c59b5f65c1ac8635072ae00af72c9a93700390
Kosphere <37622283+Kosphere@users.noreply.github.com>
2018-05-07 20:47:38 +0800

    Create lab6.js

---
+++ js/lab6.js
@@ -0,0 +1,18 @@
+let str = "";
+let i = 0;
+let j = 0;
+for (i = 0;i<countries.length;i++){
+	str += '<div class = "item"><h2>'+
+	countries[i].name+
+	'</h2><p>'+countries[i].continent+'</p>'+'<div class = "inner-box"><h3>Cities</h3>';
+	for(j = 0;j<countries[i].cities.length;j++){
+		str += countries[i].cities[j]+'<br/>';
+	}
+	str += '</div><div class = "inner-box"><h3>Populaar Photos</h3>';
+	for(j = 0;j<countries[i].photos.length;j++){
+		str += '<img class = "photo" src = "images/'+countries[i].photos[j]+'"/>';
+	}
+	str += '</div><button>Visit</button></div>';
+}
+
+document.getElementsByClassName('flex-container justify')[0].innerHTML = str;
