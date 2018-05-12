77fe2df500d52d0817d234f70860678d06ec900d
Kosphere <37622283+Kosphere@users.noreply.github.com>
2018-05-13 02:45:26 +0800

    Update demo.js

--- images/demo.js
+++ images/demo.js
@@ -15,11 +15,31 @@ function canvasTextAutoLine(str,canvas,initX,initY,lineHeight){
         if(i==str.length-1){
             ctx.fillText(str.substring(lastSubStrIndex,i+1),initX,initY);
         }
+		
     }
   }
+function canvash(str,canvas,initX,initY,lineHeight){
+    var lineWidth = 0;
+    var canvasWidth = canvas.width-100; 
+    var lastSubStrIndex= 0; 
+	    var ctx = canvas.getContext("2d"); 
+			ctx.font="100px Verdana";
+    for(let i=0;i<str.length;i++){
+        lineWidth+=ctx.measureText(str[i]).width; 
+        if(lineWidth>canvasWidth-initX){//减去initX,防止边界出现的问题
+            initY+=lineHeight;
+            lineWidth=0;
+            lastSubStrIndex=i;
+        }
+    }
+	canvas.height = initY + 50;
+	
+}
 function btClick(){
 
 	username = document.getElementById('name').value;
+	canvash('    “就这样被你征服，切断了所有退路”'+username+'想着心中的那个她，优哉游哉，辗转反侧，深夜时起来唱《征服》，歌声响彻复旦的整栋宿舍楼。唱到高音时突然破嗓，然后整栋楼都沸腾起来……',c,10,150,100);
+
 	canvasTextAutoLine('    “就这样被你征服，切断了所有退路”'+username+'想着心中的那个她，优哉游哉，辗转反侧，深夜时起来唱《征服》，歌声响彻复旦的整栋宿舍楼。唱到高音时突然破嗓，然后整栋楼都沸腾起来……',c,10,150,100);
 
 	var image = new Image();
@@ -33,5 +53,4 @@ document.getElementById('test').appendChild(image);
 var c=document.getElementById("myCanvas");
 var cxt=c.getContext("2d");
 c.width = 0.7*document.body.offsetWidth;
-c.height = 100%;
 var username;
